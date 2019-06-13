3f2881060751073d38ac89fa11a2f323373e99b0
zhangkunjsww <zhangkunjsww@dangdang.com>
2019-06-13 10:51:45 +0800

    eureka server cluster config

--- ad-eureka/src/main/java/com/zkyne/advert/AdEurekaApplication.java
+++ ad-eureka/src/main/java/com/zkyne/advert/AdEurekaApplication.java
@@ -2,11 +2,13 @@ package com.zkyne.advert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Eureka Server
  * @author zkyne
  */
+@EnableEurekaServer
 @SpringBootApplication
 public class AdEurekaApplication {
 
